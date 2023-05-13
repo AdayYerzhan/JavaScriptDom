5eec7e693de3f4b133508aa557022da365c64001
Yerzhan <yerzhan.aday@gmail.com>
2023-05-14 00:04:19 +0600

    Workin with Class for MenuCard

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 window.addEventListener('DOMContentLoaded', () => {
 
     // Tabs
@@ -112,7 +113,7 @@ window.addEventListener('DOMContentLoaded', () => {
         modal.classList.add('show');
         modal.classList.remove('hide');
         document.body.style.overflow = 'hidden';
-        clearInterval(modalTimerId);
+        // clearInterval(modalTimerId);
     }
 
     function closeModal() {
@@ -142,7 +143,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
 
-    const modalTimerId = setTimeout(openModal, 15000);
+    // const modalTimerId = setTimeout(openModal, 15000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -150,7 +151,7 @@ window.addEventListener('DOMContentLoaded', () => {
             window.removeEventListener('scroll', showModalByScroll);
         }
     }
-    
+
     window.addEventListener('scroll', showModalByScroll);
 
     // modal.addEventListener('click', (e) => {
@@ -163,6 +164,73 @@ window.addEventListener('DOMContentLoaded', () => {
     //     }
     // });
 
+  
 
+
+
+    //  Использеум классы для карточек
+    
+    const menuDB = [
+        {
+            src: "img/tabs/vegy.jpg",
+            alt: 'vegy',
+            title: 'Меню "Фитнес"',
+            descr: 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            price: 229
+        },
+        {
+            src: "img/tabs/elite.jpg",
+            alt: 'elite',
+            title: 'Меню “Премиум”',
+            descr: 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            price: 550
+        },
+        {
+            src: "img/tabs/post.jpg",
+            alt: 'post',
+            title: 'Меню "Постное"',
+            descr: 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            price: 430
+        },
+    ];
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src="${this.src}" alt="${this.alt}">
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    menuDB.forEach(item => {
+        new MenuCard(item.src, item.alt, item.title, item.descr, item.price, '.menu .container').render();
+    });
 
 }); 
